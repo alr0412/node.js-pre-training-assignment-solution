c4c8cfa743cf6527ada8b8e9730a5422e7841e9b
alr0412 <ryzkovlesa332@gmail.com>
2026-04-10 18:06:38 +0300

    solve js-ts task 2

--- JS-TS/solutions/array-helpers.ts
+++ JS-TS/solutions/array-helpers.ts
@@ -3,21 +3,51 @@
 // All helpers are declared but not implemented.
 
 export function mapArray<T, R>(source: readonly T[], mapper: (item: T, index: number) => R): R[] {
-  throw new Error('mapArray: not implemented');
+  const result: R[] = [];
+  for (let i = 0; i < source.length; i++) {
+    result.push(mapper(source[i], i));
+  }
+  return result;
 }
 
 export function filterArray<T>(source: readonly T[], predicate: (item: T, index: number) => boolean): T[] {
-  throw new Error('filterArray: not implemented');
+  const result: T[] = [];
+  for (let i = 0; i < source.length; i++) {
+    if (predicate(source[i], i))
+      result.push(source[i]);
+  }
+  return result;
 }
 
 export function reduceArray<T, R>(source: readonly T[], reducer: (acc: R, item: T, index: number) => R, initial: R): R {
-  throw new Error('reduceArray: not implemented');
+  let result: R = initial;
+  for (let i = 0; i < source.length; i++) {
+    result = reducer(result, source[i], i);
+  }
+  return result;
 }
 
 export function partition<T>(source: readonly T[], predicate: (item: T) => boolean): [T[], T[]] {
-  throw new Error('partition: not implemented');
+  let tr: T[] = []
+  let fl: T[] = []
+  for (let i = 0; i < source.length; i++) {
+    if (predicate(source[i])) {
+      tr.push(source[i]);
+    } else {
+      fl.push(source[i]);
+    }
+  }
+  return [tr, fl];
 }
 
 export function groupBy<T, K extends PropertyKey>(source: readonly T[], keySelector: (item: T) => K): Record<K, T[]> {
-  throw new Error('groupBy: not implemented');
+  const result = {} as Record<K, T[]>;
+  for (let i = 0; i < source.length; i++) {
+    if (keySelector(source[i]) in result) {
+      result[keySelector(source[i])].push(source[i]);
+    } else {
+      result[keySelector(source[i])] = [source[i]] as T[];
+    }
+  }
+  return result;
 }
